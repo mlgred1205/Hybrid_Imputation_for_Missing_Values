79117ab8f955e178ad25aa0fa48c312c599a0b09
mlgred1205 <gardner.207@osu.edu>
2020-11-11 09:26:16 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,9 +1,9 @@
-install.packages(c("sourcetools", "qvalue", "limma", "missForest", "VIM", "ggplot2", "ggridges", 
-                  "car", "Hmisc", "mice", "ggfortify", "reshape2", "cluster", "stringr",
-                  "dplyr", "factoextra", "FactoMineR", "naniar", "finalfit", "impute",
-                  "pcaMethods", "imputeLCMD", "matrixTests", "ggrepel", "biomaRt", "tidyr", 
+install.packages(c("sourcetools", "missForest", "VIM", "ggplot2", "ggridges", 
+                  "car", "Hmisc", "mice", "ggfortify", "reshape2", "cluster", 
+                   "stringr", "dplyr", "factoextra", "FactoMineR", "naniar", 
+                   "finalfit", "imputeLCMD", "matrixTests", "ggrepel", "tidyr", 
                    "viridis", "hrbrthemes"))
 
 source("http://bioconductor.org/biocLite.R")
-biocLite("qvalue")
+biocLite(c("qvalue", "limma", "imput", "pcaMethods", "biomaRt")
 
