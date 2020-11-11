6756b3f75dcf6b2b8aecc0597d0d9db5437baa4e
mlgred1205 <gardner.207@osu.edu>
2020-11-11 06:58:56 -0500

    Create install.R

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages(c("sourcetools", "qvalue", "limma", "missForest", "VIM", "ggplot2", "ggridges", 
+                  "car", "Hmisc", "mice", "ggfortify", "reshape2", "cluster", "stringr",
+                  "dplyr", "factoextra", "FactoMineR", "naniar", "finalfit", "impute",
+                  "pcaMethods", "imputeLCMD", "matrixTests", "ggrepel", "biomaRt", "tidyr", 
+                   "viridis", "hrbrthemes"))
