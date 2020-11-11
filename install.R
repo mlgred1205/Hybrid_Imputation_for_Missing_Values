903401ce5b7237a6a0a736bf49728df0be9b7479
mlgred1205 <gardner.207@osu.edu>
2020-11-11 16:26:29 -0500

    Update install.R

--- install.R
+++ install.R
@@ -2,11 +2,11 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 BiocManager::install(version = "3.10")
 BiocManager::install(c("qvalue", "limma", "impute", "imputeLCMD",
-                       "pcaMethods", "biomaRt"))
+                       "pcaMethods"))
 
 
 install.packages(c("sourcetools", "missForest", "VIM", "ggplot2", "ggridges", 
                   "car", "Hmisc", "mice", "ggfortify", "reshape2", "cluster", 
                    "stringr", "dplyr", "factoextra", "FactoMineR", "naniar", 
                    "finalfit", "matrixTests", "ggrepel", "tidyr", 
-                   "viridis", "hrbrthemes"))
+                   "viridis", "hrbrthemes", "ggplotify"))
