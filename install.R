4fa1037c75b8705cd064adcd0d9dfb9fa745bddd
mlgred1205 <gardner.207@osu.edu>
2020-11-11 10:20:44 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,9 +1,10 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(version = "3.12")
+BiocManager::install(c("qvalue", "limma", "imput", "pcaMethods", "biomaRt")
+
 install.packages(c("sourcetools", "missForest", "VIM", "ggplot2", "ggridges", 
                   "car", "Hmisc", "mice", "ggfortify", "reshape2", "cluster", 
                    "stringr", "dplyr", "factoextra", "FactoMineR", "naniar", 
                    "finalfit", "imputeLCMD", "matrixTests", "ggrepel", "tidyr", 
                    "viridis", "hrbrthemes"))
-
-source("http://bioconductor.org/biocLite.R")
-biocLite(c("qvalue", "limma", "imput", "pcaMethods", "biomaRt")
-
